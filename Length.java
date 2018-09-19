47fdd6cd23182fba92f6e485d4ea3f5b52980036
kyang19 <42977150+kyang19@users.noreply.github.com>
2018-09-18 21:58:28 -0400

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,22 @@
+
+public class Length {
+
+	public static void main(String[] args)
+	{
+		String river = "Mississippi";
+		System.out.println(river.length());
+		
+		String salutation = "Hello, World";
+				
+		System.out.println(salutation.toUpperCase());
+		
+		//no it is not legal to print river.println()
+		
+		//arguments: "p" and "s" 
+		
+		System.out.println(river.replace("p", "s"));
+		
+		System.out.print(river.toUpperCase());
+	}
+
+}
